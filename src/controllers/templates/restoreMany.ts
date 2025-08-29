08725222607d483b759cc1b629f8f5d39e7f1b67
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-08-29 19:22:44 +0700

    feat: restore and soft delete many templates

---
+++ src/controllers/templates/restoreMany.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import handleError from "@/utils/handleError";
+import { isValidObjectId, Model } from "mongoose";
+import { Normalized } from "@/types/types";
+import pluralize from "pluralize";
+
+export const restoreMany = <T extends { isRecycled: boolean; deleteAt: Date | null }>(
+  model: Model<T>,
+  funcBeforeRes?: (data: Normalized<T>[]) => any
+) => {
+  return async (req: Request, { res }: Response) => {
+    try {
+      const { ids }: { ids?: string[] } = req.body;
+      if (!ids || ids.some((id) => !isValidObjectId(id))) {
+        res.tempClientType("Object ID").respond();
+        return;
+      }
+
+      const name = model.collection.name.slice(0, -1).toLowerCase();
+      const unUpdated = await model.find({ _id: { $in: ids } });
+      if (unUpdated.some((data) => data.isRecycled)) {
+        res.tempNotFound(name).respond();
+        return;
+      }
+
+      await model.updateMany({ _id: { $in: ids } }, { isRecycled: false, deleteAt: null }, { runValidators: true });
+
+      const updatedData = unUpdated.map((data) => ({ ...data.normalize(), isRecycled: false, deleteAt: null }));
+      if (funcBeforeRes) await funcBeforeRes(updatedData);
+
+      res
+        .body({ success: updatedData })
+        .notif(`${updatedData.length} ${pluralize(name, updatedData.length)} deleted`)
+        .respond();
+    } catch (err) {
+      handleError(err, res);
+    }
+  };
+};
---
+++ src/controllers/templates/softDeleteMany.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import handleError from "@/utils/handleError";
+import { isValidObjectId, Model } from "mongoose";
+import { Normalized } from "@/types/types";
+import { getDeleteTTL } from "@/utils/database";
+import pluralize from "pluralize";
+
+export const softDeleteMany = <T extends { isRecycled: boolean; deleteAt: Date | null }>(
+  model: Model<T>,
+  funcBeforeRes?: (data: Normalized<T>[]) => any
+) => {
+  return async (req: Request, { res }: Response) => {
+    try {
+      const { ids }: { ids?: string[] } = req.body;
+      if (!ids || ids.some((id) => !isValidObjectId(id))) {
+        res.tempClientType("Object ID").respond();
+        return;
+      }
+
+      const name = model.collection.name.slice(0, -1).toLowerCase();
+      const unUpdated = await model.find({ _id: { $in: ids } });
+      if (unUpdated.some((data) => data.isRecycled)) {
+        res.tempNotFound(name).respond();
+        return;
+      }
+
+      const deleteAt = getDeleteTTL();
+      await model.updateMany({ _id: { $in: ids } }, { isRecycled: true, deleteAt }, { runValidators: true });
+
+      const updatedData = unUpdated.map((data) => ({ ...data.normalize(), isRecycled: true, deleteAt }));
+      if (funcBeforeRes) await funcBeforeRes(updatedData);
+
+      res
+        .body({ success: updatedData })
+        .notif(`${updatedData.length} ${pluralize(name, updatedData.length)} deleted`)
+        .respond();
+    } catch (err) {
+      handleError(err, res);
+    }
+  };
+};
