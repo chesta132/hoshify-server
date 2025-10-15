8a84ad23ddc3439b33f2092cea4217775daa5bb9
Chesta Ardiona <chestapanduganteng@gmail.com>
2025-10-15 16:09:49 +0700

    refactor: remove /search/models endpoint, streamline searchMany to dynamically handle models and simplify query execution

--- src/controllers/search/searchMany.ts
+++ src/controllers/search/searchMany.ts
@@ -6,6 +6,7 @@ import { Note } from "@/services/db/Note";
 import { Todo } from "@/services/db/Todo";
 import { Schedule } from "@/services/db/Schedule";
 
+const SEARCHABLE = [Note, Todo, Schedule];
 const SEARCH_MANY_FIELDS = { note: Note.searchFields, schedule: Schedule.searchFields, todo: Todo.searchFields };
 const SEARCH_MANY_SORTS = { note: Note.sortQuery, schedule: Schedule.sortQuery, todo: Todo.sortQuery };
 
@@ -20,7 +21,7 @@ export const searchMany = async (req: Request, { res }: Response, next: NextFunc
       throw new AppError("MISSING_FIELDS", { fields: "query" });
     }
     if (!models) {
-      models = ["note", "schedule", "todo"];
+      models = SEARCHABLE.map((model) => model.modelName);
     } else if (typeof models === "string") {
       models = models.split(",") as ModelSearchable[];
     }
@@ -35,12 +36,13 @@ export const searchMany = async (req: Request, { res }: Response, next: NextFunc
       orderBy: sortBy && (sort === "asc" || sort === "desc") ? { ...SEARCH_MANY_SORTS[model], [sortBy.toString()]: sort } : SEARCH_MANY_SORTS[model],
     });
 
-    const notes = models.includes("note") && (await Note.findMany(getQuery("note")));
-    const todos = models.includes("todo") && (await Todo.findMany(getQuery("todo")));
-    const schedules = models.includes("schedule") && (await Schedule.findMany(getQuery("schedule")));
+    const queries = await Promise.all(
+      SEARCHABLE.filter((model) => models.includes(model.modelName)).map(async (model) => ({
+        [model.modelName]: await model.findMany(getQuery(model.modelName)),
+      }))
+    );
 
-    const datas = { notes, todos, schedules };
-    Object.typedKeys(datas).forEach((key) => !datas[key] && delete datas[key]);
+    const datas = Object.assign({}, ...queries);
 
     res.body({ success: datas }).respond();
   } catch (err) {
--- src/routes/searchRoutes.ts
+++ src/routes/searchRoutes.ts
@@ -1,12 +1,6 @@
-import { searchNotes } from "@/controllers/note/noteFactory";
-import { searchSchedules } from "@/controllers/schedule/scheduleFactory";
 import { searchMany } from "@/controllers/search/searchMany";
-import { searchTodos } from "@/controllers/todo/todoFactory";
 import { Router } from "express";
 
 export const searchRoutes = Router();
 
 searchRoutes.get("/", searchMany);
-searchRoutes.get("/todos", searchTodos);
-searchRoutes.get("/notes", searchNotes);
-searchRoutes.get("/schedules", searchSchedules);
